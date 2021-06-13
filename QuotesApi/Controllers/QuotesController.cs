96f49c485926797448ebc21209f588195192e3b2
ZYO\adriano <a898382@gmail.com>
2021-06-14 05:36:00 +0800

    get started

---
+++ QuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuotesApi.Data;
+using QuotesApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private QuotesDbContext _quotesDbContext;
+        public QuotesController(QuotesDbContext quotesDbContext)
+        {
+            _quotesDbContext = quotesDbContext;
+        }
+        // GET: api/<QuotesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_quotesDbContext.Quotes);
+            //return StatusCode(StatusCodes.Status200OK);
+        } 
+
+        // GET api/<QuotesController>/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            if(quote == null)
+            {
+                return NotFound("Quote Is Not Found Against this id...");
+            }
+            return Ok(quote);
+        }
+
+        // POST api/<QuotesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Quote quote)
+        {
+            _quotesDbContext.Quotes.Add(quote);
+            _quotesDbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT api/<QuotesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Quote quote)
+        {
+            var entity = _quotesDbContext.Quotes.Find(id);
+            if (entity == null)
+            {
+                return NotFound("No Record Found against this id...");
+            }
+            else
+            {
+            entity.Title = quote.Title;
+            entity.Author = quote.Author;
+            entity.Description = quote.Description;
+            entity.Type = quote.Type;
+            entity.CreatedAt = quote.CreatedAt;
+            _quotesDbContext.SaveChanges();
+            return Ok("Record Updated Successfully...");
+            }
+
+        }
+
+        // DELETE api/<QuotesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            if(quote == null)
+            {
+                return NotFound("Quote Not Found against this id...");
+            }
+            else
+            {
+            _quotesDbContext.Quotes.Remove(quote);
+            _quotesDbContext.SaveChanges();
+            return Ok("Quote Deleted...");
+            }
+
+        }
+    }
+}
